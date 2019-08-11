54a3e67649e5fea5f2ae2e1a83c98963f88c1602
John F. Mercer <john.f.mercer@gmail.com>
2019-08-11 12:22:03 -0400

    Add error handling

--- src/app/slack.service.ts
+++ src/app/slack.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
 import { Auth } from './auth';
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -15,6 +16,22 @@ export class SlackService {
     const params = new HttpParams()
                      .set('token', auth.token)
                      .set('user', auth.id);
-    return this.http.get<object>(this.USER_API_URL, {params: params});
+    return this.http.get<object>(this.USER_API_URL, {params: params})
+      .pipe(
+        retry(1),
+        catchError(this.handleError<object>('getUserInfo', {}))
+      );
   }
+
+  // NOTE: Taken from https://angular.io/tutorial/toh-pt6
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+}
 }
