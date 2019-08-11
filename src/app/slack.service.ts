6c29c7a5bf312e681c4373b965c9834d0cc9d255
John F. Mercer <john.f.mercer@gmail.com>
2019-08-11 12:01:36 -0400

    Replace dummy response with real API response

--- src/app/slack.service.ts
+++ src/app/slack.service.ts
@@ -1,18 +1,20 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Auth } from './auth';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SlackService {
+  readonly USER_API_URL: string = 'https://slack.com/api/users.info';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   getUserInfo(auth: Auth): Observable<object> {
-    return of({
-      name: 'John Mercer',
-      title: 'Cat Herder'
-    });
+    const params = new HttpParams()
+                     .set('token', auth.token)
+                     .set('user', auth.id);
+    return this.http.get<object>(this.USER_API_URL, {params: params});
   }
 }
