a187fa0189e979fe3e7fd98aed25b6ce79d4862c
John F. Mercer <john.f.mercer@gmail.com>
2019-08-11 15:05:57 -0400

    Add integration test

--- src/app/slack.service.spec.ts
+++ src/app/slack.service.spec.ts
@@ -1,15 +1,47 @@
-import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TestBed, getTestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { SlackService } from './slack.service';
+import { Auth } from './auth';
 
 describe('SlackService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [HttpClientTestingModule]
-  }));
+  let injector: TestBed;
+  let service: SlackService;
+  let httpMock: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    })
+    injector = getTestBed();
+    service = injector.get(SlackService);
+    httpMock = injector.get(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
+  });
 
   it('should be created', () => {
-    const service: SlackService = TestBed.get(SlackService);
     expect(service).toBeTruthy();
   });
+
+  it('should return an Observable<object>', () => {
+    const dummyAuth: Auth = {
+      token: 'foo',
+      id: 'bar'
+    };
+    const dummyUser: object = {
+      name: 'Peter Venkman'
+    }
+
+    service.getUserInfo(dummyAuth).subscribe(user => {
+      expect(user).toEqual(dummyUser);
+    });
+
+    const req = httpMock.expectOne(`${service.USER_API_URL}`);
+    expect(req.request.method).toBe('GET');
+    req.flush(dummyUser);
+  });
+
 });
